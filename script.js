0166680e8894269a68ba670da2b010a262da506f
Mattsptc <mateusptc@gmail.com>
2022-09-05 16:11:34 -0300

    responsividade adicionada e js quase todo pronto

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-let botao = document.getElementById('button')
 
-botao.addEventListener('click', () => {
-    console.log('clicou')
-})
+function convertPokemonToLI(pokemon) {
+  return `        <li class="${pokemon.type}">
+    <span class="number">${pokemon.pokemonNumber}</span>
+    <span class="name">${(pokemon.name)}</span>
+    <div class="detail">
+       
+        <ol class="types">
+${pokemon.types.map((type) => `<li class='type ${type}'>${type}</li> `).join('')}
+        </ol>
+        <img src="${pokemon.photo}" alt="${pokemon.name}">
+</div>
+</li>`;
+}
+
+const pokemonsList = document.getElementById("pokemonsList");
+
+pokeAPI.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map((pokemon) => convertPokemonToLI(pokemon)).join('');
+  pokemonsList.innerHTML = newHtml
+
+});
